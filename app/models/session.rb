886ac194d0168e3805ec53ba0e00ea0d47394d1d
simao martins <simaopmartins@gmail.com>
2025-09-29 01:16:22 +0100

    fixed lint issues

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,5 +4,4 @@ class Session < ApplicationRecord
   def self.sweep(time = 1.hour)
     where(updated_at: ...time.ago).or(where(created_at: ...2.days.ago)).delete_all
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :collections, dependent: :destroy
-
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
