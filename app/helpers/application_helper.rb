be4cb4652fe53752ad2c3181683e7d0e9ed1c749
simao martins <simaopmartins@gmail.com>
2025-10-05 16:45:21 +0100

    fixed the modal in index, added colors to conditions like in cardmarket

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,21 @@ module ApplicationHelper
       true
     end
   end
+
+  def condition_badge(condition)
+    colors = {
+      "mint" => "bg-green-500",
+      "near_mint" => "bg-green-400",
+      "excellent" => "bg-blue-400",
+      "good" => "bg-yellow-400",
+      "lightly_played" => "bg-orange-400",
+      "played" => "bg-orange-500",
+      "poor" => "bg-red-500"
+    }
+
+    color = colors[condition] || "bg-gray-400"
+
+    content_tag(:span, condition.titleize,
+      class: "inline-block px-2 py-1 rounded text-xs font-semibold text-white #{color}")
+  end
 end
