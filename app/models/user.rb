22e089617943a847badecd77938768a0b41e2088
simao martins <simaopmartins@gmail.com>
2025-10-15 19:19:51 +0100

    fixed password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :collections, dependent: :destroy
+
+  validates :email_address, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, allow_blank: true
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
